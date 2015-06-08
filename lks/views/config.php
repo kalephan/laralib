8ae2b70a20e75e9c170079c52ccb31eac6786477
Kale Phan <kalephan@gmail.com>
2015-06-08 10:20:22 +0700

    add body-class module

---
+++ lks/views/config.php
@@ -0,0 +1,16 @@
+<?php
+return [
+    'name' => 'engine',
+    
+    'css' => [
+        'bootstrap' => '/a/css/bootstrap.min.css',
+    ],
+    
+    'js' => [
+        // Don't change us:
+        'jquery' => '/a/js/jquery.min.js',
+        'lks' => '/a/js/lks.js',
+
+        'bootstrap' => '/a/js/bootstrap.min.js',
+    ]
+];
