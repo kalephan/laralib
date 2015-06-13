766fdb21eb9c2b7e712c200e9ff7154c0b24cf9b
Kale Phan <kalephan@gmail.com>
2015-06-13 08:45:11 +0700

    add taxonomy module

---
+++ taxonomy/database/migrations/2015_06_11_100300_taxonomy_v0001_dev1.php
@@ -0,0 +1,40 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class TaxonomyV0001Dev1 extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vocabulary', function ($table) {
+            $table->string('id', 32)->primary();
+            $table->string('title', 256);
+            $table->boolean('active')->default(1);
+        });
+        
+        Schema::create('taxonomy', function ($table) {
+            $table->increments('id');
+            $table->string('vocabulary_id', 32);
+            $table->string('title', 256);
+            $table->integer('parent')->default(0)->unsigned();
+            $table->boolean('active')->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('taxonomy');
+        Schema::drop('vocabulary');
+    }
+}
